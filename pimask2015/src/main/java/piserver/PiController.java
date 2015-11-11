fd7f1a05e9d2f329f5dc76bb86807217f6e9d3c3
sylvestor88 <sylvestor.george@sjsu.edu>
2015-11-11 11:33:06 -0800

    Seperated Main method in Application Class

--- pimask2015/src/main/java/piserver/PiController.java
+++ pimask2015/src/main/java/piserver/PiController.java
@@ -2,26 +2,19 @@ package piserver;
 
 import java.util.ArrayList;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class PiController {
 
 	ArrayList<NetworkDevice> deviceList = new ArrayList<NetworkDevice>();
-	
-	@RequestMapping("/")
-	String getHome()
-	{
-		return "Testing with Kalyan!!!";
-	}
 	
 	@RequestMapping(value = "/find_device", method = RequestMethod.GET, produces = "application/json")
 	public @ResponseBody ResponseEntity<ArrayList<NetworkDevice>> listDevices() {
@@ -44,10 +37,5 @@ public class PiController {
 		}
 		
 		return new ResponseEntity<ArrayList<NetworkDevice>>(HttpStatus.NOT_FOUND);
-	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SpringApplication.run(PiController.class, args);
 	}
 }
