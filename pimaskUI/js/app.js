703c2555c7b53df0f9bc9dc351c66226945aede2
manasi-costnomics <manasi.deshpande@costnomics.com>
2015-11-20 22:02:27 -0800

    Edit/Configure Devices form

--- pimaskUI/js/app.js
+++ pimaskUI/js/app.js
@@ -13,14 +13,46 @@ var app = angular.module('PiMask',['ngRoute'])
 	.when('/', {
 		redirectTo:'/connectedDevices'
 	})
+	.when('/configureDevices', {
+		templateUrl: 'templates/configure-devices.html',
+		controller: 'ConfigureDevicesController',
+		controllerAs: 'configureDevicesCtrl'
+	})
+	.when('/editDevices', {
+		templateUrl: 'templates/edit-devices.html',
+		controller: 'EditDevicesController',
+		controllerAs: 'editDevicesCtrl'
+	})
 });
 
 app.controller('FindDevicesController', function($scope, $http){
-	 $http.get('http://localhost:8080/pimask/find_devices')
-	 .success(function(response){
+	// $http.get('http://localhost:8080/pimask/find_devices')
+	// .success(function(response){
+	 	var response = [
+	 					{"ip": "130.65.100.1", "hostname": "NewPi1"},
+	 					{"ip": "130.65.100.2", "hostname": "NewPi2"},
+	 					{"ip": "130.65.100.3", "hostname": "newPi3"}
+	 							];
 	 	$scope.names = response;
-	 })
-	 .error(function(){
-	 	alert("error");
-	 });
+	// })
+	// .error(function(){
+	 //	alert("error");
+	// });
+});
+
+app.controller('ShowDevicesController', function($scope, $http){
+	var response = [{"hostname": "Pi1", "ip": "130.65.200.1"},
+					{"hostname": "Pi2", "ip": "130.65.200.2"},
+					{"hostname": "Pi3", "ip": "130.65.200.3"}];
+	$scope.names = response;
+});
+
+app.controller('ConfigureDevicesController', function($scope, $http){
+	var response = {};
+	$scope.names = response;
+});
+
+app.controller('EditDevicesController', function($scope, $http){
+	var response = {};
+	$scope.names = response;
 });
